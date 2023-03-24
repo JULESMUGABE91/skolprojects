fb77e14ed4301a2ca83a9875f26b5de300aa1d61
Kibugenza Didier <kibugenzad@gmail.com>
2023-03-24 15:42:32 +0200

    fixed socket

--- dashboard/src/app/utils/socketIO.js
+++ dashboard/src/app/utils/socketIO.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://cleankigaliapi.huzalabs.com", {
+const socket = io("https://myskol.rw/api", {
   transports: ["websocket"],
 });
 
